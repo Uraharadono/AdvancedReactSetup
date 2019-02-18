9732908f087e691298321cddefa5880c2dec5c4c
Muhamed Zuberovic <muhamed.zuberovic@intertek.com>
2019-02-18 16:59:34 +0100

    Figured ajax stuff out.

--- app/containers/PageTwo/index.js
+++ app/containers/PageTwo/index.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import ajax from 'common/ajax';
-import axios from 'axios';
-
-function* fetchData({ data }) {
-    try {
-        // for testing purposes this "data" object will always be empty object "{}"
-        const response = yield ajax.get('/api/users', data);
-        return response;
-    } catch (ex) {
-        // handle error here
-        console.log(ex);
-    }
-}
 
 class PageTwo extends React.Component {
     componentDidMount() {
-        // const response = ajax.get('/api/users', { });
-        // console.log(response);
-        // console.log(response.data);
-
-        const data = fetchData({});
+        let data = {};
+        ajax.get('/api/users')
+            .then((serverData) => {
+                console.log(serverData);
+                data = serverData;
+            });
+        // If you are confused why this console log is showing "undefined" or "null" educate yourself about asynchronous programming and promises in javascript
         console.log(data);
-
-        // axios.get('https://reqres.in/api/users')
-        //     .then((response) => {
-        //         // handle success
-        //         console.log(response);
-        //         console.log(response.data);
-        //     })
-        //     .catch((error) => {
-        //         // handle error
-        //         console.log(error);
-        //     })
-        //     .then(() => {
-        //         // always executed
-        //     });
     }
 
     render() {
